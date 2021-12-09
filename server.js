938137749ba3c9bfbfe8e17142ed009970cb094a
Armando Guardado <armando.guardado.ag@gmail.com>
2021-12-09 13:45:48 -0800

    Updated structure of root by adding modesl folder and adding first schema

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require("cors");
 const mongoose = require("mongoose")
+const User = require("./models/user")
 
 const app = express();
 
@@ -11,22 +12,6 @@ app.use(express.urlencoded({extended:true}))
 
 
 const port = process.env.PORT || process.env.STANDARD_PORT;
-
-
-
-const userSchema = new mongoose.Schema({
-    name: String,
-    password: String
-})
-
-const User = mongoose.model("User", userSchema)
-
-const user = new User({
-    name: "Armando",
-    password: "testagain",
-})
-
-user.save();
 
 
 app.get('/testing_backend', (req, res) => {
@@ -43,25 +28,24 @@ app.get('/testing_backend', (req, res) => {
 });
 
 app.post('/testing_backend', (req, res) =>{
-    const anotherUser = new User({
-        name:"test2",
-        password: "password2"
+    const user = new User({
+        name: "Armando",
+        password: "testagain",
     })
-    const anotherUser2 = new User({
-        name:"test3",
-        password: "password3"
-    })
-    User.insertMany([anotherUser, anotherUser2], function(err){
-        if(err){
+    
+    user.save().then((err)=>{
+        if (err) {
             console.log(err)
         }
-        console.log("Successfully saved all the users to User DB")
+        else{
+            console.log('Item was successfully inserted to db.')
+        }
     });
 
 })
 
 app.put('/testing_backend', (req, res) =>{
-  User.updateOne({_id:"61b264478085a2d3ecd34811"},{name: "Armando Guardado"}, (err)=>{
+  User.updateOne({_id:"61b270ed2aee31f8ac3ed419"},{name: "Armando Guardado"}, (err)=>{
       if (err) {
           console.log(err)
       }
@@ -73,12 +57,12 @@ app.put('/testing_backend', (req, res) =>{
 })
 
 app.delete('/testing_backend', (req, res) =>{
-    User.deleteOne({_id:"61b2647c8085a2d3ecd34815"}, (err) =>{
+    User.deleteOne({_id:"61b270ed2aee31f8ac3ed419"}, (err) =>{
         if (err) {
             console.log(err)
         }
         else{
-            console.log("You have deleted the following document with id: " + "61b2647c8085a2d3ecd34815")
+            console.log("You have deleted the following document with id: " + "61b270ed2aee31f8ac3ed419")
         }
     } )
 
