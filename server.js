f1ca73d7a6d7e77e4e7c110016e8a960b7133270
Armando Guardado <armando.guardado.ag@gmail.com>
2021-12-10 09:49:43 -0800

    Added fetch feature to access The movie DB api

--- server.js
+++ server.js
@@ -3,79 +3,73 @@ const express = require('express');
 const cors = require("cors");
 const mongoose = require("mongoose")
 const User = require("./models/user")
+const { getUpComingMovies } = require("./services")
+
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 
 const port = process.env.PORT || process.env.STANDARD_PORT;
 
 
-app.get('/testing_backend', (req, res) => {
-    User.find(function(err, users){
-        if(err){
-            console.log(err);
-        }
-        else{
-            console.log(users)
-        }
-    })
-  
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+app.get('/testing_backend', async(req, res) => {
+    let json = await getUpComingMovies();
+    res.status(200).send(json.results);
 });
 
-app.post('/testing_backend', (req, res) =>{
+app.post('/testing_backend', (req, res) => {
     const user = new User({
         name: "Armando",
         password: "testagain",
     })
-    
-    user.save().then((err)=>{
+
+    user.save().then((err) => {
         if (err) {
             console.log(err)
         }
-        else{
+        else {
             console.log('Item was successfully inserted to db.')
         }
     });
 
 })
 
-app.put('/testing_backend', (req, res) =>{
-  User.updateOne({_id:"61b270ed2aee31f8ac3ed419"},{name: "Armando Guardado"}, (err)=>{
-      if (err) {
-          console.log(err)
-      }
-      else{
-          console.log("Sucessfully updated the document")
-      }
-  })
-
-})
-
-app.delete('/testing_backend', (req, res) =>{
-    User.deleteOne({_id:"61b270ed2aee31f8ac3ed419"}, (err) =>{
+app.put('/testing_backend', (req, res) => {
+    User.updateOne({ _id: "61b270ed2aee31f8ac3ed419" }, { name: "Armando Guardado" }, (err) => {
         if (err) {
             console.log(err)
         }
-        else{
+        else {
+            console.log("Sucessfully updated the document")
+        }
+    })
+
+})
+
+app.delete('/testing_backend', (req, res) => {
+    User.deleteOne({ _id: "61b270ed2aee31f8ac3ed419" }, (err) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
             console.log("You have deleted the following document with id: " + "61b270ed2aee31f8ac3ed419")
         }
-    } )
+    })
 
 })
 
 const options = {
-    useNewUrlParser:true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
 
 }
 
 
-mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, options).then(() =>{
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, options).then(() => {
     console.log("Database connection successfull");
     app.listen(port, () => console.log(`Listening on port ${port}`));
 
